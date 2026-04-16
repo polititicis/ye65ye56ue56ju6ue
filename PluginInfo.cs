317dcef925741d439417b61ce5c7d65889d86b42
polititicis <polititicis@gmail.com>
2026-04-16 13:32:50 +0100

    Update plugin GUID and name for CurlixModChecker

---
+++ PluginInfo.cs
@@ -0,0 +1,14 @@
+namespace LibrePad
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.Curlix.gorillatag.CMC";
+        public const string Name = "CurlixModChecker";
+        public const string Version = "1.0.0";
+
+        public const string ClientResourcePath = "LibrePad.Resources";
+    }
+}
